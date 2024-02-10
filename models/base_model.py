c38be90067572c457770799bab23c38d6f68d6c6
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-11 00:19:24 +0200

    10-2-2024

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class BaseModel(object):
     """docstring for BaseModel"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a Base.
 
         Args:
@@ -15,9 +15,20 @@ class BaseModel(object):
             created_at: created_at of the Base.
             updated_at: iupdated_atd of the Base.
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key not in ["__class__", "created_at", "updated_at"]:
+                    setattr(self, key, value)
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return class name, id, and the dictionary"""

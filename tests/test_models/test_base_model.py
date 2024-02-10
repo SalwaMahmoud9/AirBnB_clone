ea20097739c860e1f0fa0fcbdb2bc1d41e02837d
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-11 00:54:46 +0200

    10-2-2024

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -43,6 +43,18 @@ class TestBaseModel_instance(unittest.TestCase):
     def test_storage_new(self):
         self.assertIn(BaseModel(), models.storage.all().values())
 
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        model = BaseModel()
+        model.id = "123456"
+        model.created_at = model.updated_at = dt
+        modelstr = model.__str__()
+        self.assertIn("[BaseModel] (123456)", modelstr)
+        self.assertIn("'id': '123456'", modelstr)
+        self.assertIn("'created_at': " + dt_repr, modelstr)
+        self.assertIn("'updated_at': " + dt_repr, modelstr)
+
 
 class TestBaseModel_save(unittest.TestCase):
     """Unittests save BaseModel class."""

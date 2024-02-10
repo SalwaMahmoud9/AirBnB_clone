88415248bcb54bea1af76668d432cf6a5f8c8368
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-10 23:31:19 +0200

    10-2-2024

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a base model class."""
 import json
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -15,7 +15,7 @@ class BaseModel(object):
             created_at: created_at of the Base.
             updated_at: iupdated_atd of the Base.
         """
-        self.id = uuid.uuid4()
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 

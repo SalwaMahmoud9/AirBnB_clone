cc130ad7c2c5843ee62635cad35d39225d400c7c
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-11 22:33:23 +0200

    10-2-2024

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,63 +1,63 @@
 #!/usr/bin/python3
-"""Defines unittests for models/state.py."""
+"""Defines unittests for models/city.py."""
 import os
 import models
 import unittest
 from datetime import datetime
 from time import sleep
-from models.state import State
+from models.city import City
 
 
-class TestState_instance(unittest.TestCase):
-    """Unittest State class."""
+class TestCity_instance(unittest.TestCase):
+    """Unittest City class."""
 
-    def test_state_instance(self):
-        self.assertEqual(State, type(State()))
+    def test_city_instance(self):
+        self.assertEqual(City, type(City()))
 
     def test_id_is_str(self):
-        self.assertEqual(str, type(State().id))
+        self.assertEqual(str, type(City().id))
 
     def test_created_at_is_datetime(self):
-        self.assertEqual(datetime, type(State().created_at))
+        self.assertEqual(datetime, type(City().created_at))
 
     def test_updated_at_is_datetime(self):
-        self.assertEqual(datetime, type(State().updated_at))
+        self.assertEqual(datetime, type(City().updated_at))
 
     def test_uniqueIds(self):
-        model1 = State()
-        model2 = State()
+        model1 = City()
+        model2 = City()
         self.assertNotEqual(model1.id, model2.id)
 
     def test_diff_created_at(self):
-        model1 = State()
+        model1 = City()
         sleep(1)
-        model2 = State()
+        model2 = City()
         self.assertLess(model1.created_at, model2.created_at)
 
     def test_diff_updated_at(self):
-        model1 = State()
+        model1 = City()
         sleep(1)
-        model2 = State()
+        model2 = City()
         self.assertLess(model1.updated_at, model2.updated_at)
 
     def test_storage_new(self):
-        self.assertIn(State(), models.storage.all().values())
+        self.assertIn(City(), models.storage.all().values())
 
     def test_str_representation(self):
         dt = datetime.today()
         dt_repr = repr(dt)
-        model = State()
+        model = City()
         model.id = "123456"
         model.created_at = model.updated_at = dt
         modelstr = model.__str__()
-        self.assertIn("[State] (123456)", modelstr)
+        self.assertIn("[City] (123456)", modelstr)
         self.assertIn("'id': '123456'", modelstr)
         self.assertIn("'created_at': " + dt_repr, modelstr)
         self.assertIn("'updated_at': " + dt_repr, modelstr)
 
 
-class TestState_save(unittest.TestCase):
-    """Unittests save State class."""
+class TestCity_save(unittest.TestCase):
+    """Unittests save City class."""
     @classmethod
     def setUp(self):
         try:
@@ -77,14 +77,14 @@ class TestState_save(unittest.TestCase):
             pass
 
     def test_save(self):
-        model = State()
+        model = City()
         sleep(1)
         first_updated_at = model.updated_at
         model.save()
         self.assertLess(first_updated_at, model.updated_at)
 
     def test_more_saves(self):
-        model1 = State()
+        model1 = City()
         sleep(1)
         updated_at1 = model1.updated_at
         model1.save()
@@ -95,22 +95,22 @@ class TestState_save(unittest.TestCase):
         self.assertLess(updated_at2, model1.updated_at)
 
     def test_update_file(self):
-        model = State()
+        model = City()
         model.save()
-        modelId = "State." + model.id
+        modelId = "City." + model.id
         with open("file.json", "r") as f:
             self.assertIn(modelId, f.read())
 
 
-class TestState_to_dict(unittest.TestCase):
-    """Unittests to_dict State class."""
+class TestCity_to_dict(unittest.TestCase):
+    """Unittests to_dict City class."""
 
     def test_to_dict_type(self):
-        model = State()
+        model = City()
         self.assertTrue(dict, type(model.to_dict()))
 
     def test_to_dict_correct_data(self):
-        model = State()
+        model = City()
         self.assertIn("id", model.to_dict())
         self.assertIn("created_at", model.to_dict())
         self.assertIn("updated_at", model.to_dict())

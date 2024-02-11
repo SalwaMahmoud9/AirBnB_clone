f0240926e8e18da45c8dfaa1f381a9813a83f0ad
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-02-11 22:45:12 +0200

    salwa 11-2-2024

--- console.py
+++ console.py
@@ -13,7 +13,23 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+    
 
 
 class HBNBCommand(cmd.Cmd):
@@ -81,23 +97,6 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
             
-    def parse(arg):
-        curly_braces = re.search(r"\{(.*?)\}", arg)
-        brackets = re.search(r"\[(.*?)\]", arg)
-        if curly_braces is None:
-            if brackets is None:
-                return [i.strip(",") for i in split(arg)]
-            else:
-                lexer = split(arg[:brackets.span()[0]])
-                retl = [i.strip(",") for i in lexer]
-                retl.append(brackets.group())
-                return retl
-        else:
-            lexer = split(arg[:curly_braces.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(curly_braces.group())
-            return retl
-    
     def do_show(self, arg):
         """Prints the string representation of 
         an instance based on the class name and id."""

7dfcb8dfab0a7bf71c406eb31a502cecc18dd3e0
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-11 22:10:11 +0200

    10-2-2024

--- models/user.py
+++ models/user.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""user class, subclass of BaseModel
-"""
+"""User"""
 
 from models.base_model import BaseModel
 import json
 
 
 class User(BaseModel):
-    '''subclass of BaseModel class'''
+    """child from BaseModel class"""
 
     email = ""
     password = ""

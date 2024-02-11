5a2eaa0c827e8b931701bc9fad71526d23684cba
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-02-11 10:34:57 +0200

    salwa 11-2-2024

---
+++ console.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Console."""
+import cmd
+
+
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HBnB command.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+    def emptyline(self):
+        """Shouldn’t execute anything."""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
